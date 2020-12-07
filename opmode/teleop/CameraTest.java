f1aac5e676cd65b4499bf82b9701a37546894dfd
Lucas Lee <lucasclee11@gmail.com>
2020-12-07 17:03:05 -0500

    minor changes

--- opmode/teleop/CameraTest.java
+++ opmode/teleop/CameraTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmode.auton;
+package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 /* Copyright (c) 2019 FIRST. All rights reserved.
  *
@@ -50,7 +50,6 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
 public class CameraTest extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
