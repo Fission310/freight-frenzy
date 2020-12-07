64aeccb6cf11a372de1d0293827c3c937b67a11e
Lucas Lee <lucasclee11@gmail.com>
2020-12-07 17:02:08 -0500

    camera test added

---
+++ opmode/auton/MoveTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.opmode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.hardware.Drivetrain;
+import org.firstinspires.ftc.teamcode.hardware.Motor;
+
+@Autonomous(name="Test")
+public class MoveTest {
+}
--- opmode/teleop/MotorTest.java
+++ opmode/teleop/MotorTest.java
@@ -4,16 +4,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.hardware.Motor;
+import org.firstinspires.ftc.teamcode.hardware.ServoTest;
 
 @TeleOp(name="MotorTest", group="Test")
 public class MotorTest extends LinearOpMode{
 
     private Motor motor = new Motor(this);
+    private ServoTest servo = new ServoTest(this);
 
     @Override
     public void runOpMode() throws InterruptedException{
 
         motor.init(hardwareMap);
+        servo.init(hardwareMap);
 
         waitForStart();
 
@@ -24,6 +27,12 @@ public class MotorTest extends LinearOpMode{
             }
             else if(gamepad1.b){
                 motor.backwards();
+            }
+            else if(gamepad1.x){
+                servo.moveToPos(0.9);
+            }
+            else if(gamepad1.y){
+                servo.moveBy(-0.1);
             }
         }
 
