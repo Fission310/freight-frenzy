bc6f9d2bfa99e540ad408d249d0297d325487d77
Lucas-Lee11 <lucasclee11@gmail.com>
2022-01-10 16:36:36 -0500

    acquirer

---
+++ hardware/Acquirer.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Acquirer extends Mechanism{
+
+    private DcMotor pasta;
+
+    @Override
+    public void init(HardwareMap hwMap) {
+        pasta = hwMap.dcMotor.get("pasta");
+
+        //TODO reverse if necessary
+        //pasta.setDirection(DcMotorSimple.Direction.REVERSE);
+        pasta.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void acquire(){
+        pasta.setPower(1);
+    }
+
+    public void reverse(){
+        pasta.setPower(-1);
+    }
+}
