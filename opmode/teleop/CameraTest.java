31d4a6d821f343d4241d542682cdb27530461f6c
Lucas-Lee11 <lucasclee11@gmail.com>
2022-01-20 16:10:56 -0500

    camera setup

---
+++ opmode/teleop/CameraTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Camera;
+
+@TeleOp
+public class CameraTest extends LinearOpMode {
+
+    Camera camera = new Camera(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        camera.init(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            telemetry.addData("test", "test");
+            telemetry.update();
+            camera.listDetections();
+        }
+    }
+}
