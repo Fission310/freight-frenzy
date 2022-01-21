56c1c567b569942de5e97d38a785dc61a01262d3
pserb <paulserbanescu3@gmail.com>
2022-01-21 12:06:35 -0500

    remove unused imports

--- opmode/auton/CameraAuto.java
+++ opmode/auton/CameraAuto.java
@@ -3,12 +3,8 @@ package org.firstinspires.ftc.teamcode.opmode.auton;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.hardware.BarcodeDetector;
 import org.firstinspires.ftc.teamcode.hardware.Webcam;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 @Autonomous
 public class CameraAuto extends LinearOpMode {
@@ -17,6 +13,7 @@ public class CameraAuto extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Webcam webcam = new Webcam(this);
         webcam.init(hardwareMap);
+        webcam.setPipeline();
 
 
         waitForStart();
