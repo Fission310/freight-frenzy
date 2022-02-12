e9df4f4573ac8969bfa9bff3f60b9d9282548d49
pserb <paulserbanescu3@gmail.com>
2022-02-11 19:16:20 -0500

    tuning...

--- drive/DriveConstants.java
+++ drive/DriveConstants.java
@@ -34,7 +34,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0.4, 0, 1.4, 13);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(14, 0, 4, 13);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0135;
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
     public static double kA = 0;
-    public static double kStatic = 0.1;
+    public static double kStatic = 0;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -86,10 +86,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 45;
+    public static double MAX_VEL = 35;
     public static double MAX_ACCEL = 52.48291908330528;
-    public static double MAX_ANG_VEL = Math.toRadians(231.31152000000003);
-    public static double MAX_ANG_ACCEL = Math.toRadians(231.31152000000003);
+    public static double MAX_ANG_VEL = Math.toRadians(167);
+    public static double MAX_ANG_ACCEL = Math.toRadians(167);
 
 
     public static double encoderTicksToInches(double ticks) {
