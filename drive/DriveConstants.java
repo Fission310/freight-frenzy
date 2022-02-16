c5eee1f8484e696f04b09b8e12856b42dedbf8a7
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-02-15 23:41:18 -0500

    TUNING.

--- drive/DriveConstants.java
+++ drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1.0093; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 10.625; // in
+    public static double TRACK_WIDTH = 13.1; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
