7bc607053044163638689ee04dfb8aa84ee936f7
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-02-20 01:00:15 -0500

    Create SingleMain.java

---
+++ opmode/teleop/SingleMain.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.Acquirer;
+import org.firstinspires.ftc.teamcode.hardware.Slides;
+import org.firstinspires.ftc.teamcode.hardware.SlidesTest;
+
+@Config
+@TeleOp (name = "SingleMain", group = "Test")
+public class SingleMain extends LinearOpMode {
+
+    private SampleMecanumDrive rrDrive;
+
+    // ===== TESTING ===== //
+    private Slides slides = new Slides(this);
+    private Acquirer acquirer = new Acquirer(this);
+    private SlidesTest slidesTest = new SlidesTest(this);
+    // =================== //
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        rrDrive = new SampleMecanumDrive(hardwareMap);
+
+        slides.init(hardwareMap);
+        acquirer.init(hardwareMap);
+        slidesTest.init(hardwareMap);
+
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            rrDrive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            slides.loop(gamepad1);
+            acquirer.loop(gamepad1);
+            slidesTest.loop(gamepad1);
+
+            telemetry.addData("Cup pos", slides.getCupPos());
+            telemetry.addData("Cup timer", slides.getCupTimer());
+            telemetry.addData("Motor position", slidesTest.getPos());
+            telemetry.addData("Target position", slidesTest.getTarget());
+            telemetry.addData("BUsy?", slidesTest.testMotor.isBusy());
+            telemetry.update();
+
+        }
+    }
+
+}
