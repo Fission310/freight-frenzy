25d46f1915ac2ad9558dd23ed7ace7db8c71a977
Lucas-Lee11 <lucasclee11@gmail.com>
2022-02-20 23:02:06 -0500

    slides pid?

--- hardware/SlidesTest.java
+++ hardware/SlidesTest.java
@@ -1,14 +1,14 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.control.PIDFController;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 
 @Config
 public class SlidesTest extends Mechanism {
@@ -18,12 +18,20 @@ public class SlidesTest extends Mechanism {
     public static int SLIDE_EXTEND_POS = 700;
     public static int SLIDE_RETRACT_POS = 0;
 
+    public static double WHEEL_RADIUS = 1.81102;
+    public static double TICKS_PER_REV = 145.1;
+    public static double GEAR_RATIO = 1.0;
+
+    public static double targetPosition;
+
+    public static PIDCoefficients coeffs = new PIDCoefficients(0.1, 0, 0);
+    PIDFController controller;
+
     public enum MotorState {
         MOTOR_EXTENDED,
         MOTOR_RETRACTED
     }
-
-    MotorState motorState = MotorState.MOTOR_RETRACTED;
+    MotorState motorState;
 
     public SlidesTest(LinearOpMode opMode) { this.opMode = opMode; }
 
@@ -31,35 +39,38 @@ public class SlidesTest extends Mechanism {
     public void init(HardwareMap hwMap) {
         testMotor = hwMap.get(DcMotorEx.class, "testMotor");
         testMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        testMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         testMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        testMotor.setTargetPositionTolerance(10);
+        motorState = MotorState.MOTOR_RETRACTED;
+        targetPosition = SLIDE_RETRACT_POS;
+        controller = new PIDFController(coeffs);
     }
 
-    // 537 ticks per rev
     public void extend() {
-        testMotor.setPower(0.5);
-        testMotor.setTargetPosition(SLIDE_EXTEND_POS ); // 1 revolution
-        testMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        controller.setTargetPosition(SLIDE_EXTEND_POS);
     }
     public void retract() {
-        testMotor.setPower(0.5);
-        testMotor.setTargetPosition(SLIDE_RETRACT_POS);
-        testMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        controller.setTargetPosition(SLIDE_RETRACT_POS);
     }
 
-    public int getPos() {
-        return testMotor.getCurrentPosition(); // in ticks
+    public double getPosition() {
+
+        return encoderTicksToInches(testMotor.getCurrentPosition());
+    }
+    public double getVelocity() {
+
+        return encoderTicksToInches(testMotor.getVelocity());
     }
 
-    public int getTarget() {
-        return testMotor.getTargetPosition(); // in ticks
+    public void update(){
+        double power = controller.update(getPosition(), getVelocity());
+        testMotor.setPower(power);
     }
 
 
 
     public void loop(Gamepad gamepad1) {
-//        if(!testMotor.isBusy()) testMotor.setPower(0);
 
         switch (motorState) {
             case MOTOR_RETRACTED:
@@ -77,5 +88,20 @@ public class SlidesTest extends Mechanism {
             default:
                 motorState = MotorState.MOTOR_RETRACTED;
         }
+
+        update();
+    }
+
+    public static double encoderTicksToInches(double ticks) {
+        return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+    }
+
+    public static double rpmToVelocity(double rpm) {
+        return rpm * GEAR_RATIO * 2 * Math.PI * WHEEL_RADIUS / 60.0;
+    }
+
+    public static double getMotorVelocityF(double ticksPerSecond) {
+        // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
+        return 32767 / ticksPerSecond;
     }
 }
