32a18fff5a650fc5432982d2a18926ee0290706e
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-02-21 15:51:47 -0500

    distance sensor test

---
+++ hardware/mechanisms/CarriageSensor.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.hardware.mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class CarriageSensor extends Mechanism {
+    private DistanceSensor sensor;
+
+    public CarriageSensor(LinearOpMode opMode) {this.opMode = opMode; }
+
+    @Override
+    public void init(HardwareMap hwMap) { sensor = hwMap.get(DistanceSensor.class, "distanceSensor"); }
+
+    public double distanceCM() {
+        return sensor.getDistance(DistanceUnit.CM);
+    }
+    public double distanceMM() {
+        return sensor.getDistance(DistanceUnit.MM);
+    }
+}
