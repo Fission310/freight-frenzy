2f12c66fbc76c47e6f596bb78449b9ed59987331
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-02-28 08:12:56 -0500

    add all servos

--- hardware/mechanisms/OdoLift.java
+++ hardware/mechanisms/OdoLift.java
@@ -9,6 +9,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 @Config
 public class OdoLift extends Mechanism {
     private Servo wallOdoServo;
+    private Servo hubOdoServo;
+    private Servo perpOdoServo;
 
     public static double LIFT_HEIGHT = 1;
     public static double LOWER_HEIGHT = 0;
@@ -18,10 +20,20 @@ public class OdoLift extends Mechanism {
     @Override
     public void init(HardwareMap hwMap) {
         wallOdoServo = hwMap.get(Servo.class, "wallOdoServo");
+        hubOdoServo = hwMap.get(Servo.class, "hubOdoServo");
+        perpOdoServo = hwMap.get(Servo.class, "perpOdoServo");
     }
 
-    public void lift() { wallOdoServo.setPosition(LIFT_HEIGHT); }
-    public void lower() { wallOdoServo.setPosition(LOWER_HEIGHT); }
+    public void lift() {
+        wallOdoServo.setPosition(LIFT_HEIGHT);
+        hubOdoServo.setPosition(LIFT_HEIGHT);
+        perpOdoServo.setPosition(LIFT_HEIGHT);
+    }
+    public void lower() {
+        wallOdoServo.setPosition(LOWER_HEIGHT);
+        hubOdoServo.setPosition(LOWER_HEIGHT);
+        perpOdoServo.setPosition(LOWER_HEIGHT);
+    }
 
     public void loop(Gamepad gamepad1) {
         if (gamepad1.dpad_up) {
