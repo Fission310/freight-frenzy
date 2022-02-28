423ba14fbee7a1a9614075d2e9e3e84481e8afc8
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-02-28 08:14:45 -0500

    add mechanisms

--- hardware/MeccRobot.java
+++ hardware/MeccRobot.java
@@ -1,31 +1,26 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.hardware.mechanisms.Acquirer;
-import org.firstinspires.ftc.teamcode.hardware.mechanisms.CarouselTest;
-import org.firstinspires.ftc.teamcode.hardware.mechanisms.CarriageSensor;
+import org.firstinspires.ftc.teamcode.hardware.mechanisms.Arm;
+import org.firstinspires.ftc.teamcode.hardware.mechanisms.Carousel;
 import org.firstinspires.ftc.teamcode.hardware.mechanisms.Drivetrain;
 import org.firstinspires.ftc.teamcode.hardware.mechanisms.Mechanism;
-import org.firstinspires.ftc.teamcode.hardware.mechanisms.Slides;
-import org.firstinspires.ftc.teamcode.hardware.mechanisms.SlidesTest;
+import org.firstinspires.ftc.teamcode.hardware.mechanisms.OdoLift;
 
 public class MeccRobot extends Mechanism {
 
     // init all mechanisms
     private Drivetrain dt = new Drivetrain(opMode);
     private Acquirer acquirer = new Acquirer(opMode);
-    private CarouselTest supaspeed = new CarouselTest(opMode);
-    private CarriageSensor carriageSensor = new CarriageSensor(opMode);
-    private Slides slides = new Slides(opMode);
-    private SlidesTest slidesTest = new SlidesTest(opMode);
+    private Carousel carousel = new Carousel(opMode);
+    private OdoLift odoLift = new OdoLift(opMode);
+    private Arm arm = new Arm(opMode);
 
-//    private MultipleTelemetry telemetry;
 
     public MeccRobot(LinearOpMode opMode) { this.opMode = opMode; }
 
@@ -33,23 +28,18 @@ public class MeccRobot extends Mechanism {
     public void init(HardwareMap hwMap) {
         dt.init(hwMap);
         acquirer.init(hwMap);
-        supaspeed.init(hwMap);
-        carriageSensor.init(hwMap);
-        slides.init(hwMap);
-        slidesTest.init(hwMap);
+        carousel.init(hwMap);
+        odoLift.init(hwMap);
+        arm.init(hwMap);
     }
 
-    public void telemetry(Telemetry telemetry) {
-        telemetry.addData("Distance MM", carriageSensor.distanceMM());
-        telemetry.addData("Distance CM", carriageSensor.distanceCM());
-    }
 
     public void loop(Gamepad gamepad1) {
         dt.loop(gamepad1);
         acquirer.loop(gamepad1);
-        supaspeed.loop(gamepad1);
-        slides.loop(gamepad1);
-        slidesTest.loop(gamepad1);
+        carousel.loop(gamepad1);
+        odoLift.loop(gamepad1);
+        arm.loop(gamepad1);
     }
 
 }
