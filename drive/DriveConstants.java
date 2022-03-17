fdc713e147ccb83245652b38bd7bc5ca9448ecdc
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-03-17 00:05:30 -0400

    tuning v100

--- drive/DriveConstants.java
+++ drive/DriveConstants.java
@@ -34,8 +34,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(34, 0, 16,
-            13.6);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(10, 0, 3.5,
+            13.1);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -47,7 +47,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1.1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 11; // in
+    public static double TRACK_WIDTH = 13.08; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
--- drive/StandardTrackingWheelLocalizer.java
+++ drive/StandardTrackingWheelLocalizer.java
@@ -35,8 +35,8 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 5.56; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 3.85; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 5.549912; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 3.81; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
     public static double X_MULTIPLIER = 1.0121108424;// Multiplier in the X direction
