dcc59e9bbdecbf9b1e7f77bdf59053f57b6a3f18
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-03-25 20:30:51 -0400

    telemetry >:(

--- opmode/teleop/SingleMain.java
+++ opmode/teleop/SingleMain.java
@@ -16,14 +16,15 @@ public class SingleMain extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
 
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        //telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
             robot.loop(gamepad1);
-            robot.telemetry(telemetry);
-            telemetry.update();
+            //robot.telemetry(telemetry);
+
+            //telemetry.update();
         }
     }
 
