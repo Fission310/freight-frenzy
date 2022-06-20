f75e18ff36d417dfcf8566b0ef0d6c5b7c8bf038
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-06-19 23:38:37 -0400

    carousel

--- hardware/mechanisms/Carousel.java
+++ hardware/mechanisms/Carousel.java
@@ -1,49 +1,39 @@
 package org.firstinspires.ftc.teamcode.hardware.mechanisms;
 
-import com.stuyfission.fissionlib.motion.MotionProfiledDcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.stuyfission.fissionlib.util.Mechanism;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.apache.commons.math3.geometry.euclidean.twod.Line;
-
 @Config
 public class Carousel extends Mechanism {
-    private MotionProfiledDcMotor carousel;
+    private DcMotorEx carousel;
 
-    private static final double WHEEL_RADIUS = 1.415;
-    private static final double GEAR_RATIO = 1.0;
-    private static final double TICKS_PER_REV = 537.6;
-
-    public static double MAX_VEL = 10;
-    public static double MAX_ACCEL = 10;
-
-    public static double kP = 0;
-    public static double kI = 0;
-    public static double kD = 0;
-    public static double kF = 0;
-
-    public static double ROTATE_POS = 10;
+    public static double POWER = 0.27;
+    public static double AUTO_POWER = 0.2;
 
     public Carousel(LinearOpMode opMode) { this.opMode = opMode; }
 
     @Override
     public void init(HardwareMap hwMap) {
-        carousel = new MotionProfiledDcMotor(hwMap, "carousel");
-        carousel.setWheelConstants(WHEEL_RADIUS, GEAR_RATIO, TICKS_PER_REV);
-        carousel.setMotionConstraints(MAX_VEL, MAX_ACCEL);
-        carousel.setPIDCoefficients(kP, kI, kD, kF);
-        carousel.setTargetPosition(0);
+        carousel = hwMap.get(DcMotorEx.class, "carousel");
     }
 
     public void rotate() {
-        carousel.setTargetPosition(ROTATE_POS);
+        carousel.setPower(POWER);
     }
-
+    public void rotateAUTO() {
+        carousel.setPower(AUTO_POWER);
+    }
+    public void reverse() {
+        carousel.setPower(-POWER);
+    }
+    public void reverseAUTO() {
+        carousel.setPower(-AUTO_POWER);
+    }
     public void stop() {
         carousel.setPower(0);
     }
@@ -52,6 +42,8 @@ public class Carousel extends Mechanism {
     public void loop(Gamepad gamepad) {
         if (gamepad.right_bumper) {
             rotate();
+        } else if (gamepad.left_bumper) {
+            reverse();
         } else {
             stop();
         }
