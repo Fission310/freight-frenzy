cf6b7fcfe4600c2ef449f967310c6bb35019a90e
Paul Serbanescu <paulserbanescu3@gmail.com>
2022-06-21 01:11:05 -0400

    rename

--- opmode/teleop/PairedMain.java
+++ opmode/teleop/PairedMain.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.hardware.MeccRobot;
 
-@TeleOp (name = "PairedMain", group = "test")
+@TeleOp (name = "Paired Main", group = "_main")
 public class PairedMain extends LinearOpMode {
 
     private MeccRobot robot = new MeccRobot(this);
